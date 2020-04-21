b17589a5beb94eb18fa8a9cff82a4a1268fc9e89
Peter Burnett <peterburnett@catalyst-au.net>
2020-04-21 12:39:15 +1000

    Fixed typo in settings

--- settings.php
+++ settings.php
@@ -2,14 +2,13 @@
 
     defined('MOODLE_INTERNAL') || die();
 
-    // Admin Controls: 
+    // Admin Controls:
     // https://docs.moodle.org/dev/Admin_settings
 
-    if ($hassitconfig) {
+    if ($hassiteconfig) {
         $settings = new admin_settingpage('tool_test', get_string('pluginname', 'tool_test'));
         $ADMIN->add('tools', $settings);
 
-        
-        //$settings->add(new admin_setting_heading('tool_test/heading', 'Test Heading', ''));
-        //$settings->add(new admin_setting_configselect('forum_displaymode', get_string('displaymode', 'forum'),get_string('configdisplaymode', 'forum'), FORUM_MODE_NESTED, forum_get_layout_modes()));
+
+        $settings->add(new admin_setting_heading('tool_test/heading', 'Test Heading', ''));
     }
